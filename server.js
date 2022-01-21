50cc8cd702d7cb889d5c6f9e72f74c9d0bc51f0d
SrBiscuit <78145316+SrBiscuits@users.noreply.github.com>
2022-01-21 18:20:17 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+
+const Websocket = require('ws');
+const express = require('express');
+const app=express()
+
+app.get("/",function(res,req){
+    res.send("working")
+})
+
+app.listen(process.env.PORT||5000);
+
+const PORT=5000;
+
+const wsServer = new Websocket.Server({
+    port: PORT
+})
+
+wsServer.on('connection',function(socket){
+    //Feedback
+    console.log("a client just connected");
+    //Behavior
+    socket.on('message',function(msg){
+        console.log("Recieved message from client: "+msg);
+        //Reenviar
+        socket.send("Resend message: "+msg);
+
+        //Broadcast
+        wsServer.clients.forEach(function(client){
+            client.send("Reenviado a todos: "+msg);
+        })
+    })
+})
+
+console.log((new Date())+"Server is listening on port "+PORT);
